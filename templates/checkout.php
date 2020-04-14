6469ddce43f1d4da756471de0714faa726aa191e
cunidev <raffaele.tranquillini@gmail.com>
2020-04-14 16:54:41 +0200

    use templates for all pages

--- templates/checkout.php
+++ templates/checkout.php
@@ -1,40 +1,12 @@
+<?php
+$title = "Checkout";
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>EatKraken</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Khula">
-  <link rel="stylesheet" href="../css/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <script src="https://kit.fontawesome.com/4978f3ffc8.js" crossorigin="anonymous"></script>
-</head>
+<?php include("widgets/common_head.php"); ?>
 <body>
 
-<header>
-<nav class="navbar navbar-inverse navbar-custom">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#" style="color: white">
-        <i class="fas fa-hamburger"></i> 
-        <span class="navbar-logo"><b>EATKRAKEN</b></span>
-      </a>
-    </div>
-    <div class="collapse navbar-collapse navbar-right" id="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a class="btn btn-sm btn-primary checkout-button">Confirm</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-</header>
+<?php include("widgets/navbar.php"); ?>
 
 
 <div class="container-fluid text-center mainbody">    
@@ -96,24 +68,8 @@
   </div>
 </div>
 
-<div class="footer-basic">
-  <footer>
-      <div class="social">
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-twitter"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-      </div>
 
-      <ul class="list-inline">
-          <li class="list-inline-item"><a href="#">Home</a></li>
-          <li class="list-inline-item"><a href="#">Services</a></li>
-          <li class="list-inline-item"><a href="#">About</a></li>
-          <li class="list-inline-item"><a href="#">Terms</a></li>
-          <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-      </ul>
-      <p class="copyright">Company Name © 2018</p>
-  </footer>
-</div>
+<?php include("widgets/footer.php"); ?>
 
 </body>
 </html>
