d20de59a73e62e27b4d4affdfa7bef9b992ff84e
cunidev <raffaele.tranquillini@gmail.com>
2020-04-14 17:05:57 +0200

    make product a template

--- templates/product.php
+++ templates/product.php
@@ -3,26 +3,33 @@
 <?php include("widgets/common_head.php"); ?>
 <body>
 
-
 <?php include("widgets/navbar.php"); ?>
-
 
 <div class="mainbody"> 
     <div class="home-inner container">
       <div class="caption text-center">
-        <h1>Greta Burger</h1>
-        <h3 class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te </h3>
-        <button id="buttonID">Add to cart</button>
+        <h1><?= $product_name ?></h1>
+        <p class="lead"><?= $product_desc ?></p>
+        <h3><b>€<?= $price ?></b><small>/piece</small></h3>
+        <button id="btn-cart-add">Add to cart</button>
       </div>
     </div>
 </div>
 
-
 <div class="container-fluid threeColumnsHome">
   <div class="row">
-    <div class="col-xs-12 col-sm-4 text-center "><h2>Allergenes</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
-    <div class="col-xs-12 col-sm-4 text-center "><h2>Ingredients</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
-    <div class="col-xs-12 col-sm-4 text-center "><h2>Nutritional facts</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
+    <div class="col-xs-12 col-sm-4 text-center ">
+      <h2>Allergenes</h2>
+      <?= $product_allergenes ?>
+    </div>
+    <div class="col-xs-12 col-sm-4 text-center ">
+      <h2>Ingredients</h2>
+      <?= $product_ingredients ?>
+    </div>
+    <div class="col-xs-12 col-sm-4 text-center ">
+      <h2>Nutritional facts</h2>
+      <?= $product_nutri_facts ?>
+      </div>
   </div>
 </div>
 
