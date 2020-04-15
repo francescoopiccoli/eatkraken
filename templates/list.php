50821972063c44134ea4c3e11a5b6ee9955c182b
cunidev <raffaele.tranquillini@gmail.com>
2020-04-15 10:22:42 +0200

    list template

--- templates/list.php
+++ templates/list.php
@@ -1,3 +1,52 @@
+<?php
+$title  = "Search results";
+$cities = array(
+  array("id" => 1, "name" => "Bolzano"),
+  array("id" => 2, "name" => "Merano"),
+  array("id" => 3, "name" => "Rovereto"),
+  array("id" => 4, "name" => "Trento"),
+);
+
+$options = array(
+  array("id" => 1, "name" => "Fresh"),
+  array("id" => 2, "name" => "Gluten-free"),
+  array("id" => 3, "name" => "Lactose-free"),
+  array("id" => 4, "name" => "Vegan"),
+  array("id" => 5, "name" => "Vegetarian"),
+);
+
+$categories = array(
+  array("id" => 5, "name" => "Asian"),
+  array("id" => 6, "name" => "Asian > Chinese"),
+  array("id" => 7, "name" => "Asian > Sushi"),
+  array("id" => 1, "name" => "Buns"),
+  array("id" => 2, "name" => "Buns > Burgers"),
+  array("id" => 3, "name" => "Buns > Kebab"),
+  array("id" => 8, "name" => "Pizza"),
+);
+
+
+$results = array(
+  array(
+    "id" => 123,
+    "name" => "Pizza Margherita",
+    "price" => 15,
+    "picture_url" => "https://cdn-media.italiani.it/site-caserta/2019/01/pizza-con-ananas-caserta-2.jpg",
+    "restaurant_id" => 1,
+    "restaurant_name" => "Pizzeria Scarsa SNC"
+  ),
+
+  array(
+    "id" => 234,
+    "name" => "Durum Kebab",
+    "price" => 7,
+    "picture_url" => "https://tourismembassy.com/media/multimedia/images/45e1696726edab87cfec4dae6049d63e.jpg",
+    "restaurant_id" => 2,
+    "restaurant_name" => "KeBZ"
+  )
+);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include("widgets/common_head.php"); ?>
@@ -11,7 +60,13 @@
       <br><br>
       <h4>Deliverable to</h4>
       <div class="form-group">
-        <select name="" id="" class="form-control"></select>
+        <select name="" id="" class="form-control">
+          <?php
+            foreach ($cities as $city) {
+              echo("<option value=\"{$city["id"]}\">{$city["name"]}</option>");
+            }
+          ?>
+        </select>
         <input type="text" name="" id="" class="form-control" placeholder="Address">
       </div>
       <br>
@@ -19,22 +74,25 @@
       <input type="datetime-local" name="" id="" class="form-control">
       <br>
       <h4>In category</h4>
-      <select name="" id="" class="form-control"></select>
+      <select name="" id="" class="form-control">
+        <option value="0" selected>All</option>
+        <?php
+          foreach ($categories as $category) {
+            echo("<option value=\"{$category["id"]}\">{$category["name"]}</option>");
+          }
+        ?>
+        <option value="-1">Others</option>
+      </select>
       <br>
       <h4>Show only</h4>
       <div class="">
-        <input type="checkbox" name="" id="">
-        <label for="">Gluten-free</label>
-        <br>
-        <input type="checkbox" name="" id="">
-        <label for="">Lactose-free</label>
-        <br>
-        <input type="checkbox" name="" id="">
-        <label for="">Vegan</label>
-        <br>
-        <input type="checkbox" name="" id="">
-        <label for="">Mystery boxes</label>
-        <br>
+        <?php
+          foreach($options as $option) {
+            echo("<input type=\"checkbox\" name=\"opt_{$option["id"]}\"> ".
+            "<label for=\"opt_{$option["id"]}\">{$option["name"]}</label>".
+            "<br>");
+          }
+        ?>
       </div>
     </div>
     <div class="col-sm-9 text-left"> 
@@ -43,21 +101,26 @@
       
       <hr>
       <div class="row">
-        <a class="col-md-2 col-sm-4 food-card" href="pizza">
+        <?php
+        foreach($results as $result) {
+        ?>
+        <a class="col-md-2 col-sm-4 food-card" href="/product.php?id=<?= $result["id"] ?>">
           <div class="panel panel-default">
-            <div class="panel-body list-thumb" style="background-image: url('https://cdn-media.italiani.it/site-caserta/2019/01/pizza-con-ananas-caserta-2.jpg'); background-size: cover; min-width: 100px; min-height: 120px;">
-              
+            <div class="panel-body list-thumb" style="background-image: url('<?= $result["picture_url"] ?>'); background-size: cover; min-width: 100px; min-height: 120px;">
             </div>
             <div class="panel-footer">
-              <b>Pizza Margherita</b> 
-              <span class="text-right">$$$</span>
+              <b><?= $result["name"] ?></b> 
+              <span class="text-right">€<?= $result["price"] ?></span>
               <br>
-              <small>Pizzeria Scarsa SNC</small>
+              <small><?= $result["restaurant_name"] ?></small>
               
             </div>
           </div>
-        </div>
+        </a>
       </a>
+      <?php
+      }
+      ?>
     </div>
   </div>
 </div>
