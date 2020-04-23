43f0b6840d45d9e141c679394538b5fa4c6796b4
cunidev <raffaele.tranquillini@gmail.com>
2020-04-23 13:14:25 +0200

    WIP: orders page template

--- templates/admin/orders.php
+++ templates/admin/orders.php
@@ -1,117 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>EatKraken</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Khula">
-  <link rel="stylesheet" href="../../css/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <script src="https://kit.fontawesome.com/4978f3ffc8.js" crossorigin="anonymous"></script>
-</head>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/templates/widgets/common_head.php"); ?>
 <body>
-
-<header>
-<nav class="navbar navbar-inverse navbar-custom">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">
-        <i class="fas fa-hamburger"></i> 
-        <span class="navbar-logo"><b>EATKRAKEN</b></span>
-      </a>
-    </div>
-    <div class="collapse navbar-collapse navbar-right" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li><a href="#"><b>HOME</b></a></li>
-        <li><a href="#"><b>ABOUT</b></a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a><i class="fas fa-shopping-cart"></i></a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-</header>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/templates/widgets/navbar.php"); ?>
 
 
-<div class="container-fluid text-center mainbody">    
-  <div class="row content">
-    <div class="col-sm-12 text-left"> 
-      <h2>Pending orders</h2>
-      <p>By accepting an order, you agree to deliver it on time at the specified conditions.</p>
-      
-      <table class="table">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">User</th>
-            <th scope="col">Order</th>
-            <th scope="col">Due total</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">18355</th>
-            <td>
-              <b>Francesco Ricci</b><br>
-              Via Alto Adige, 5 <br>
-              00100 Bolzano (BZ) <br>
-              <a href="tel:+393310123456">+39 331 012 3456</a>
-            </td>
-            <td>
-              3x <b>Greta Burger</b> (€5)<br>
-              <i>"Without onions, thanks!!!!!"</i>
-              <br>
-              2x <b>Large Coke</b> (€2.5)
-              <hr>
-              <b>Home delivery</b> (€5)
-            </td>
-            <td>€25</td>
-            <td>
-              Deliver within <b>40 minutes</b><br>
-              <button class="btn btn-success btn-sm">
-                Accept
-              </button> 
-              <button class="btn btn-danger btn-sm">
-                Refuse
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+  <div class="container-fluid text-center mainbody">    
+    <div class="row content">
+      <div class="col-sm-12 text-left"> 
+        <h2>Pending orders</h2>
+        <p>By accepting an order, you agree to deliver it on time at the specified conditions.</p>
+        
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">User</th>
+              <th scope="col">Order</th>
+              <th scope="col">Due total</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">18355</th>
+              <td>
+                <b>Francesco Ricci</b><br>
+                Via Alto Adige, 5 <br>
+                00100 Bolzano (BZ) <br>
+                <a href="tel:+393310123456">+39 331 012 3456</a>
+              </td>
+              <td>
+                3x <b>Greta Burger</b> (€5)<br>
+                <i>"Without onions, thanks!!!!!"</i>
+                <br>
+                2x <b>Large Coke</b> (€2.5)
+                <hr>
+                <b>Home delivery</b> (€5)
+              </td>
+              <td>€25</td>
+              <td>
+                Deliver within <b>40 minutes</b><br>
+                <button class="btn btn-success btn-sm">
+                  Accept
+                </button> 
+                <button class="btn btn-danger btn-sm">
+                  Refuse
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
-      <h2>Past orders</h2>
-      see above, just instead of "actions" one sees "status" label (accepted/refused)
-    </div>
-  </div>
-</div>
-
-<div class="footer-basic">
-  <footer>
-      <div class="social">
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-twitter"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
+        <h2>Past orders</h2>
+        see above, just instead of "actions" one sees "status" label (accepted/refused)
       </div>
+    </div>
+  </div>
 
-      <ul class="list-inline">
-          <li class="list-inline-item"><a href="#">Home</a></li>
-          <li class="list-inline-item"><a href="#">Services</a></li>
-          <li class="list-inline-item"><a href="#">About</a></li>
-          <li class="list-inline-item"><a href="#">Terms</a></li>
-          <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-      </ul>
-      <p class="copyright">Company Name © 2018</p>
-  </footer>
-</div>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/templates/widgets/footer.php"); ?>
+
 
 </body>
 </html>
