26acf26a208ce3d74208c2462359a2a0d48e86f3
francescopiccoli <xfrancescopiccolix@gmail.com>
2020-04-24 15:31:22 +0200

    changes

--- libs/database.php
+++ libs/database.php
@@ -34,20 +34,20 @@ function db_get_cities() {
 
 function db_get_categories() {
     return array(
-        array("id" => 5, "name" => "Asian"),
-        array("id" => 6, "name" => "Asian > Chinese"),
-        array("id" => 7, "name" => "Asian > Sushi"),
-        array("id" => 1, "name" => "Buns"),
-        array("id" => 2, "name" => "Buns > Burgers"),
-        array("id" => 3, "name" => "Buns > Kebab"),
-        array("id" => 8, "name" => "Pizza"),
+        array("code" => 5, "name" => "Asian"),
+        array("code" => 6, "name" => "Asian > Chinese"),
+        array("code" => 7, "name" => "Asian > Sushi"),
+        array("code" => 1, "name" => "Buns"),
+        array("code" => 2, "name" => "Buns > Burgers"),
+        array("code" => 3, "name" => "Buns > Kebab"),
+        array("code" => 8, "name" => "Pizza"),
       );
 }
 
 function db_get_dishes($city, $cat, $deadline, $flags) {
     return array(
         array(
-          "id" => 123,
+          "code" => 123,
           "name" => "Pizza Margherita",
           "price" => 15,
           "picture_url" => "https://cdn-media.italiani.it/site-caserta/2019/01/pizza-con-ananas-caserta-2.jpg",
@@ -56,7 +56,7 @@ function db_get_dishes($city, $cat, $deadline, $flags) {
         ),
       
         array(
-          "id" => 234,
+          "code" => 234,
           "name" => "Durum Kebab",
           "price" => 7,
           "picture_url" => "https://tourismembassy.com/media/multimedia/images/45e1696726edab87cfec4dae6049d63e.jpg",
@@ -64,6 +64,12 @@ function db_get_dishes($city, $cat, $deadline, $flags) {
           "restaurant_name" => "KeBZ"
         )
       );
+}
+
+
+function db_get_product($productCode){
+    $product = db_simple_query("select * from dishes where code = '$productCode'");
+    return $product;
 }
 
 
--- product.php
+++ product.php
@@ -1,29 +1,31 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/libs/database.php");
-
-
-
 // extract from DB -> https://phpdelusions.net/pdo#prepared
-$connection = new PDO($GLOBALS['db_pdo_data']);
 
-$stmt = $pdo->prepare('SELECT * FROM users WHERE email = ? AND status=?');
-$stmt->execute([$email, $status]);
-$user = $stmt->fetch();
 
-try {
-    if($query = $connection->query($query_text)) 
-        return $query->fetchAll();
-    else
-        return false;
-} catch (Exception $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-$connection = null;
+$product = db_get_product($productCode);
 
-$title = "Order Greta Burger - EatKraken";
-$product_name = "Greta Burger";
-$product_desc = "sssss";
+$title = "Order" . $product["name"] . "- EatKraken";
+$product_name = $product["name"];
+$product_desc = $product["description"];;
+$product_price = $product["price"];
+$product_allergenes = $product["allergenes"];
+$product_ingredients = $product["ingredients"];
+$product_nutri_carbs = $product["nutri_carbs"];
+$product_nutri_fats = $product["nutri_fats"];
+$product_nutri_kcal = $product["nutri_kcal"];
+$product_nutri_proteins = $product["nutri_proteins"];
+$product_flag_gluten_free = $product["flag_gluten_free"];
+$product_flag_lactose_free = $product["flag_lactose_free"];
+$product_flag_vegan = $product["flag_vegan"];
+$product_flag_fresh = $product["flag_fresh"];
+$product_flag_zero_waste = $product["flag_zero_waste"];
+$product_image_url = $product["image_url"];
+
+
+
+
+
 
 require_once($_SERVER['DOCUMENT_ROOT'] . "/templates/product.php");
 
