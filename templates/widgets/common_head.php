7df6669d280c3f141144d45eaa30663d27c6078a
francescopiccoli <xfrancescopiccolix@gmail.com>
2020-04-24 12:18:14 +0200

    changes

--- templates/widgets/common_head.php
+++ templates/widgets/common_head.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Khula">
     <link rel="stylesheet" href="/css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Acme' rel='stylesheet'>
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
