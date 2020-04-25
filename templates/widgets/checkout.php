0b51b25b41bf5d6d22dbe7eda34bd80a7f0c87ed
cunidev <raffaele.tranquillini@gmail.com>
2020-04-25 19:39:06 +0200

    wip: early checkout widget

--- templates/widgets/checkout.php
+++ templates/widgets/checkout.php
@@ -3,12 +3,20 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/libs/session.php");
 
 if(is_logged_in()) {
 ?>
+<li>
+    <a class="btn btn-danger" href="/restaurant/logout.php">Log out</a>
+</li>
 
 <?php
 } else {
 ?>
 
-<li><a href="/checkout.php"><i class="fas fa-shopping-cart"></i></a></li>
+<li class="navbar-text checkout-widget">
+    5 items, €50
+</li>
+<li class="checkout-widget">
+    <a href="/checkout.php"><i class="fas fa-shopping-cart"></i></a>
+</li>
 
 <?php
 }
