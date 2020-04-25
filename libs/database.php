af34ccadd5e87de980ddf99d85ad1ae5f6c89c7c
francescopiccoli <xfrancescopiccolix@gmail.com>
2020-04-25 10:58:05 +0200

    working image

--- libs/database.php
+++ libs/database.php
@@ -76,7 +76,7 @@ function db_get_dishes($city, $cat, $deadline, $flags) {
 
 
 function db_get_product($productCode){
-    $product = db_simple_query("select * from dishes where code = '$productCode'");
+    $product = db_simple_query("select * from dishes where code = $productCode");
     if(count($product) > 0)
         return $product[0];
     else
