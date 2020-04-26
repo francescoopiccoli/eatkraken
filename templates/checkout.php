5ca20f8b727f6971e980fa89acbea117724075db
cunidev <raffaele.tranquillini@gmail.com>
2020-04-26 12:10:17 +0200

    WIP checkout

--- templates/checkout.php
+++ templates/checkout.php
@@ -4,73 +4,81 @@ $title = "Checkout";
 <!DOCTYPE html>
 <html lang="en">
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/templates/widgets/common_head.php"); ?>
-<body>
+  <body>
 
-<?php include($_SERVER['DOCUMENT_ROOT'] . "/templates/widgets/navbar.php"); ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/templates/widgets/navbar.php"); ?>
 
 
-<div class="container-fluid text-center mainbody">    
-  <div class="row content checkout-main">
-    <div class="col-sm-12 text-left"> 
+    <div class="container-fluid text-center mainbody">    
+      <div class="row content checkout-main">
+        <div class="col-sm-12 text-left"> 
 
-      <div class="row checkout-total-row">
-        <div class="col-sm-8">
-          <b>Deliver to:</b>
-          <br>
-          <span id="address">AAA BBB CCC</span> (<a href="">edit</a>)
-        </div>
-        
-        <div class="col-sm-2 text-center">
-          Shipping
-          <h2>€5</h2>
-        </div>
-        <div class="col-sm-2 text-center">
-          Total
-          <h2>€55</h2>
+          <div class="row checkout-total-row">
+            <div class="col-sm-8">
+              <b>Deliver to:</b>
+              <br>
+              <span id="address">AAA BBB CCC</span> (<a href="#" onclick="editAddress()">edit</a>)
+            </div>
+            
+            <div class="col-sm-2 text-center">
+              Shipping
+              <h2>€5</h2>
+            </div>
+            <div class="col-sm-2 text-center">
+              Total
+              <h2>€55</h2>
+            </div>
+          </div>
+
+          <h2>Pizzeria Scarsa</h2>
+          
+          <div class="row checkout-results-row">
+            <div class="col-sm-2">
+              <img class="thumbnail-small" src="https://cdn-media.italiani.it/site-caserta/2019/01/pizza-con-ananas-caserta-2.jpg">
+            </div>
+            <div class="col-sm-5">
+              <h3>Pizza Margherita</h3>
+              <p>1350Kcal</p>
+              <small><i id="message">message</i> (<a href="#" onclick="editNotes()">edit</a>)</small>
+            </div>
+            <div class="col-sm-3">
+              <select name="" id="" class="form-control">
+                <option value="">0 (remove)</option>
+                <option value="" selected>1x</option>
+                <option value="">2x</option>
+                <option value="">3x</option>
+                <option value="">4x</option>
+                <option value="">5x</option>
+              </select>
+              <br>
+              <select name="" id="" class="form-control">
+                <option value="">Home delivery (+€5)</option>
+                <option value="">Eat in (free)</option>
+              </select>
+            </div>
+            <div class="col-sm-2 text-center">
+              <h3>€50</h3>
+              +warnings?
+            </div>
+          </div>
+
+
+
         </div>
       </div>
-
-      <h2>Pizzeria Scarsa</h2>
-      
-      <div class="row checkout-results-row">
-        <div class="col-sm-2">
-          <img class="thumbnail-small" src="https://cdn-media.italiani.it/site-caserta/2019/01/pizza-con-ananas-caserta-2.jpg">
-        </div>
-        <div class="col-sm-5">
-          <h3>Pizza Margherita</h3>
-          <p>1350Kcal</p>
-          <small><i>message</i> (<a href="">edit</a>)</small>
-        </div>
-        <div class="col-sm-3">
-          <select name="" id="" class="form-control">
-            <option value="">0 (remove)</option>
-            <option value="" selected>1x</option>
-            <option value="">2x</option>
-            <option value="">3x</option>
-            <option value="">4x</option>
-            <option value="">5x</option>
-          </select>
-          <br>
-          <select name="" id="" class="form-control">
-            <option value="">Home delivery (+€5)</option>
-            <option value="">Eat in (free)</option>
-          </select>
-        </div>
-        <div class="col-sm-2 text-center">
-          <h3>€50</h3>
-          +warnings?
-        </div>
-      </div>
-
-
-
     </div>
-  </div>
-</div>
 
 
-<?php include($_SERVER['DOCUMENT_ROOT'] . "/templates/widgets/footer.php"); ?>
-
-</body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/templates/widgets/footer.php"); ?>
+    <script>
+      function editMessage() {
+        prompt("Enter custom message");
+      }
+      function editAddress() {
+        address = prompt("Enter delivery address");
+        $("#address").text(address);
+      }
+    </script>
+  </body>
 </html>
 
