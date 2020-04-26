08741bbd5c66d8627a0a7d20b504fb74b3279729
cunidev <raffaele.tranquillini@gmail.com>
2020-04-26 10:14:35 +0200

    implement checkout widget

--- libs/session.php
+++ libs/session.php
@@ -10,10 +10,28 @@ function cart_get_items() {
         return $_SESSION['cart'];
 }
 function cart_add_item($id) {
+    $price = db_get_item_price($id);
+    if($price <= 0)
+        return false; // invalid item
+
+    // store price for better DB performance and to avoid final price to be different from when the user added it
     if(!isset($_SESSION['cart']))
-        $_SESSION['cart'] = array($id);
+        $_SESSION['cart'] = array(
+            array("id" => $id, "price" => $price)
+        );
     else
-        array_push($_SESSION['cart'], $id);
+        array_push($_SESSION['cart'], array("id" => $id, "price" => $price));
+    
+    return true;
+}
+function cart_get_total() {
+    $total = 0;
+    if(isset($_SESSION['cart'])) {
+        foreach ($_SESSION['cart'] as $item) {
+            $total += $item["price"];
+        }
+    }
+    return $total;
 }
 
 // Restaurant login -- todo!
