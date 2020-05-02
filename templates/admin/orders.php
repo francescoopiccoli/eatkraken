e5aeb01de73bfb2d417452161f6492963daa8826
francescopiccoli <xfrancescopiccolix@gmail.com>
2020-05-02 14:59:37 +0200

    kinda working email

--- templates/admin/orders.php
+++ templates/admin/orders.php
@@ -7,6 +7,14 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/libs/database.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/libs/session.php");
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once($_SERVER['DOCUMENT_ROOT'] . "/templates/admin/PHPMailer/src/Exception.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/templates/admin/PHPMailer/src/PHPMailer.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/templates/admin/PHPMailer/src/SMTP.php");
+
+
 if(!restaurant_is_logged_in()) {
   header("Location: /");
   exit;
@@ -31,12 +39,35 @@ function get_dish($code){
 }
 
 function email(){
-$to = "xfrancescopiccolix@gmail.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: xfrancescopiccoli@gmail.com\r\n";
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.sendgrid.net';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'info.eatkraken@gmail.com';                     // SMTP username
+    $mail->Password   = 'eatkraken2020';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-mail($to,$subject,$txt,$headers);}
+    //Recipients
+    $mail->setFrom('info.eatkraken@gmail.com', 'Eatkraken');
+        $mail->addAddress('...');     // Add a recipient
+
+     // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Your eatkraken order';
+    $mail->Body    = 'Your order has been accepted, it will be at your place as 
+    soon as possible';
+    $mail->AltBody = 'Your order has been accepted, it will be at your place as 
+    soon as possible';
+
+    $mail->send();
+}
+catch (Exception $e) {
+  echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+}
 
 function removeElement($code){
   $connection = new PDO($GLOBALS['db_pdo_data']);
@@ -52,6 +83,7 @@ if(isset($_POST['order'])) {
     $stmt = $connection->prepare("UPDATE orders SET status = 1  WHERE code = ? AND restaurant = ?");
     if($stmt->execute([$_POST['order'], restaurant_get_logged_id()])){
        // silently accept
+    	email();
     }
   }
 
