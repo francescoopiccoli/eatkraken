72adf34104cbf5d6559511699a65d5737c373326
francescopiccoli <xfrancescopiccolix@gmail.com>
2020-05-09 18:16:19 +0200

    first implementation of manage-dishes.php

--- views/admin/manage-dishes.php
+++ views/admin/manage-dishes.php
@@ -8,63 +8,70 @@ if(!restaurant_is_logged_in()) {
 }
 ?>
 <?php
-$dishes = array(
-  array(
-    "id" => 123,
-    "name" => "Pizza Margherita",
-    "price" => 15,
-    "picture_url" => "https://cdn-media.italiani.it/site-caserta/2019/01/pizza-con-ananas-caserta-2.jpg",
-  )
-);
+function get_dishes(){
+  return db_stmt_query("select code, name, price, image_url from dishes where restaurant = ?", [restaurant_get_logged_id()]);
+}
+
+function removeElement($code){
+  
+}
+
+//not working for now
+if(isset($_POST['remove'])) {
+  $code= $dish['code'];
+  echo $code;
+  $connection = new PDO($GLOBALS['db_pdo_data'], $GLOBALS['db_username'], $GLOBALS['db_password'], array(PDO::ATTR_PERSISTENT => true));
+  $stmt = $connection->prepare("DELETE FROM dishes WHERE code = $code;");
+  $stmt->execute();
+  $connection = null;
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/views/widgets/common_head.php"); ?>
 <body>
-  <script>
-  function removeDish(id) {
-    if(confirm("Are you sure?")) {
-      // ajax POST
-      location.reload();
-    }
-  }
-  </script>
-
+  
   <?php include($_SERVER['DOCUMENT_ROOT'] . "/views/widgets/navbar.php"); ?>
 
 
   <div class="container-fluid text-center mainbody">    
     <div class="row content">
       <div class="col-sm-12 text-left"> 
-        <h1>Your dishes</h1>
-        <p>
-          Here are all the dishes in your restaurant, sorted alphabetically.
-        </p>
+        <h1 style="display: inline-block">Your dishes</h1>
+        <a href="add-dish.php" style="font-family: 'Acme'; float:right; margin-top: 20px;" class="btn btn-success btn-lg">Add dish</a>
         <br>
-        <a href="add-dish.php" class="btn btn-success btn-sm">Add</a>
-        
+        <i>To edit a dish, please delete it and insert it again.</i>
         <hr>
+
         <div class="row">
-          <?php foreach($dishes as $dish) { ?>
-          <div class="col-md-2 col-sm-4" href="">
-            <div class="panel panel-default">
-              <div class="panel-body list-thumb" style="background-image: url('<?= $dish["picture_url"]; ?>'); background-size: cover; min-width: 100px; min-height: 120px;">
-              
+          <?php 
+
+            $dishes = get_dishes();
+
+              foreach($dishes as $dish) { ?>
+              <div class="col-md-2 col-sm-4" href="">
+                <div class="panel panel-default">
+                  <div class="panel-body list-thumb" style="background-image: url('<?= $dish["image_url"]; ?>'); background-size: cover; min-width: 100px; min-height: 120px;">
+                  
+                  </div>
+                  <div class="panel-footer text-center">
+                  <a style="color:black" href="/product.php?code=<?= $dish["code"];?>" target="_blank">
+                    <b style="font-size:1.1em"><?= $dish["name"] ?></b>                     
+                  </a>
+
+                    <br>
+                    <b><?= $dish["price"] ?>€</b> 
+                    <br>
+                    <form method="post" action="manage-dishes.php">
+                    <input type="hidden" name="dish" value="<?= $dish['code']; ?>">
+                    <input type="submit" name="remove" value="Remove" class="btn btn-danger btn-sm"/>
+                    </form> 
+                  </div>
+                </div>
               </div>
-              <div class="panel-footer text-center">
-                <b><?= $dish["name"] ?></b> 
-                <br>
-                <a class="btn btn-primary btn-sm" href="/product.php?id=<?= $dish["id"]; ?>" target="_blank">
-                  View
-                </a>
-                <button class="btn btn-danger btn-sm" onclick="removeDish(<?= $dish["id"]; ?>)">
-                  Remove
-                </button>
-              </div>
-            </div>
-          </div>
-          <?php } ?>
+              <?php } ?>
         </div>
       </div>
     </div>
