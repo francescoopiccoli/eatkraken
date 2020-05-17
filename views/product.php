fb74d76116d421c6e5f344a9faeeaf38ee85d97a
giuliosciortino <sciortinogiulio0@gmail.com>
2020-05-17 22:58:19 +0200

    style product

--- views/product.php
+++ views/product.php
@@ -21,7 +21,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/libs/session.php");
             <img class="circular--landscape" id="profile-pic" src=<?=$product_image_url?>>
             <h1><?= $product_name ?></h1>
             <p class="lead"><?= $product_desc ?></p>
-            <h3><b>€<?= $product_price ?></b><small style="color: #ccc; font-weight: 500;"> / piece</small></h3>
+            <h3><b>€<?= $product_price ?></b><small id="small"> / piece</small></h3>
             <form action="/product.php?code=<?= $product_id; ?>" method="post">
               <input type="hidden" name="add">
               <button type="submit" class="homePageButton" id="cart-add-btn"<?= $product_id; ?>>
@@ -49,28 +49,28 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/libs/session.php");
     <div class="container-fluid threeColumnsHome">
       <div class="row">
         <div class="col-xs-12 col-sm-4 text-left ">
-          <h2 style="margin-top: 15px; text-align:center">Product info</h2>
+          <h2 class="centered-h2" >Product info</h2>
           <?= $product_allergenes ?>
         </div>
         <div class="col-xs-12 col-sm-4 text-center ">
-          <h2  style="margin-top: 15px;">Ingredients</h2>
+          <h2  class="centered-h2">Ingredients</h2>
           <div id="ingredients"><?= $product_ingredients ?></div>
         </div>
         <div class="col-xs-12 col-sm-4 text-left ">
-          <h2 style="margin-top: 15px; text-align:center">Nutritional facts  (x 100g)</h2>
+          <h2 class="centered-h2">Nutritional facts  (x 100g)</h2>
           <?= $product_nutri_facts ?>
           </div>
       </div>
     </div>
 
-    <div class="container-fluid justify-content-center "  style="background-color: #F0F0F0; width:100%">
-          <div class="row justify-content-center" style="padding: 30px;">
+    <div id="cont-rest" class="container-fluid justify-content-center" >
+          <div id="row-padd" class="row justify-content-center">
           <div class="col-xs-12 col-sm-4 text-center"><?php echo "<img style=\"width:100%; opacity: 0.7; border: 1px solid; border-radius: 80%\" src=\"$restaurant_image_url\">"?>
           </div>
           <div class="col-xs-12 col-sm-8 text-center">
-            <h2 style="margin-top: 30px;"> <?= $restaurant_name?></h2>
-            <hr style="border-color: #bbb; width: 30%">
-            <div style="width:100%; text-align: center;">
+            <h2 id="rest-name"> <?= $restaurant_name?></h2>
+            <hr id="hr1">
+            <div id="rest-desc">
             <i><?= $restaurant_description?></i></div>
           </div>
       </div>
